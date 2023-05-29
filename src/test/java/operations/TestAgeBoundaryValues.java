22159d184c76d3aca7143749659a7e38f3764b09
Yuliia Sokolova <65017585+juliia1988@users.noreply.github.com>
2023-05-30 01:31:51 +0200

    homework update

--- src/test/java/operations/TestAgeBoundaryValues.java
+++ src/test/java/operations/TestAgeBoundaryValues.java
@@ -4,31 +4,42 @@ import dataproviders.TestDataProvider;
 import org.example.Man;
 import org.example.Woman;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestAgeBoundaryValues {
 
+    private Woman woman;
+    private Man man;
+
+    @BeforeMethod
+    public void createWoman(){
+        woman = new Woman("Olena","Volosyna",35,"Ÿes");
+    }
+
+    @BeforeMethod
+    public void createMan(){
+        man = new Man("Oleg","Syvko",55,"Yes");
+    }
+
+
     @Test(dataProvider = "age for woman that not Retired", dataProviderClass = TestDataProvider.class)
     public void testWomanNotRetired(int age) {
-        Woman woman = new Woman("Olena","Volosyna",age,"Ÿes");
         Assert.assertNotEquals(woman.isRetired(age), age < 60 , "Retired age works with mistakes");
     }
 
     @Test(dataProvider = "age for woman that Retired", dataProviderClass = TestDataProvider.class)
     public void testWonamRetired(int age) {
-        Woman woman = new Woman("Viktoria","Kozakova",age,"No");
         Assert.assertEquals(woman.isRetired(age), age > 60 , "Retired age works with mistakes");
     }
 
     @Test(dataProvider = "age for man that not Retired", dataProviderClass = TestDataProvider.class)
     public void testManNotRetired(int age) {
-        Man man = new Man("Oleg","Syvko",age,"Ÿes");
         Assert.assertNotEquals(man.isRetired(age), age < 65 , "Retired age works with mistakes");
     }
 
     @Test(dataProvider = "age for man that Retired", dataProviderClass = TestDataProvider.class)
     public void testManRetired(int age) {
-        Man man = new Man("Evgeniy","horlov",age,"No");
         Assert.assertEquals(man.isRetired(age), age > 65 , "Retired age works with mistakes");
     }
 
